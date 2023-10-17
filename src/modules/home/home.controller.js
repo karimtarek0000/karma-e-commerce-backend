001e3bf54936835e7a77fac3d03d8ace1bf76a30
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-10-17 18:44:50 +0300

    Fix in post slider

--- src/modules/home/home.controller.js
+++ src/modules/home/home.controller.js
@@ -3,16 +3,18 @@ import { productModel } from '../../../DB/models/Product.model.js';
 import cloudinary from '../../lib/cloudinary.cloud.js';
 import { sendError } from '../../lib/sendError.js';
 
+// ------ Get all products slider --------
 export const slider = async (req, res, next) => {
   const sliderProducts = await sliderModel.find().populate([
     {
       path: 'productId',
     },
   ]);
-  if (!sliderProducts) return sendError(next, 'occured error', 400);
+  if (!sliderProducts) return sendError(next, 'Occured error while get slider', 400);
   res.status(200).json({ message: 'All products slider', sliderProducts });
 };
 
+// ------ Add new product in slider --------
 export const addProductInSlider = async (req, res, next) => {
   const { file } = req;
   const { productId, tag } = req.body;
@@ -27,6 +29,12 @@ export const addProductInSlider = async (req, res, next) => {
     folder: `${process.env.FOLDER_NAME}/home/slider`,
   });
 
+  // -------------------------- If data not saved in database --------------------------
+  async function deleteResources() {
+    await cloudinary.uploader.destroy(public_id);
+  }
+  req.catchErrorFn = deleteResources;
+
   // ------------- Save data in database -------------
   const product = {
     productId,
@@ -36,13 +44,7 @@ export const addProductInSlider = async (req, res, next) => {
 
   const sliderProduct = await sliderModel.create(product);
 
-  // -------------------------- If data not saved in database --------------------------
-  async function deleteResources() {
-    await cloudinary.uploader.destroy(public_id);
-  }
-
   if (!sliderProduct) {
-    req.catchErrorFn = deleteResources;
     return sendError(next, 'Error happend while save data', 500);
   }
 
