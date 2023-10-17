6b5fa8326853e40ebcf2bc4336e24f033cfc7bad
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-10-17 18:56:04 +0300

    Home routes

--- src/modules/home/home.routes.js
+++ src/modules/home/home.routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { errorHandler } from '../../lib/errorHandler.js';
+import { isAuth } from '../../middlewares/auth.js';
 import { validationCore } from '../../middlewares/validations.js';
 import { allowExtensionsTypes, uploadFilesWithCloud } from '../../services/uploadFiles.cloud.js';
+import { systemRoles } from '../../utils/systemRoles.js';
 import { addProductInSlider, slider } from './home.controller.js';
 import { addProductInSliderSchema } from './home.validation.js';
 
@@ -9,7 +11,7 @@ const router = Router();
 
 router
   .get('/slider', errorHandler(slider))
-  // .use(isAuth([systemRoles.ADMIN, systemRoles.SUPERADMIN]))
+  .use(isAuth([systemRoles.ADMIN, systemRoles.SUPERADMIN]))
   .post(
     '/slider',
     uploadFilesWithCloud(allowExtensionsTypes.image).single('product', 1),
