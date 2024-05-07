df729dc611534b86f04c6240266d3da851b2d643
karim <karimtarekabdelaziz@gmail.com>
2024-05-07 22:25:38 +0300

    Fix: Cart

--- src/modules/cart/cart.routes.js
+++ src/modules/cart/cart.routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { errorHandler } from '../../lib/errorHandler.js';
-import { validationCore } from '../../middlewares/validations.js';
 import { isAuth } from '../../middlewares/auth.js';
+import { validationCore } from '../../middlewares/validations.js';
+import { systemRoles } from '../../utils/systemRoles.js';
 import { addProductInCart, deleteProductFromCart, getCart } from './cart.controller.js';
 import { addCartSchema, deleteCartSchema } from './cart.validation.js';
-import { systemRoles } from '../../utils/systemRoles.js';
 
 const router = Router();
 
 router
-  .use(isAuth([systemRoles.USER]))
+  .use(isAuth([systemRoles.USER, systemRoles.ADMIN, systemRoles.SUPERADMIN]))
   .post('/', validationCore(addCartSchema), errorHandler(addProductInCart))
   .get('/', errorHandler(getCart))
   .delete('/:productId', validationCore(deleteCartSchema), errorHandler(deleteProductFromCart));
