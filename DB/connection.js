f3eaa5f44087261028990700bda3820704da1d9e
Karim Tarek abdel aziz <43219770+karimtarek0000@users.noreply.github.com>
2026-05-24 21:49:22 +0300

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,18 +1,10 @@
 import mongoose from 'mongoose';
 
-let cached = global.mongoose || { conn: null, promise: null };
-global.mongoose = cached;
-
 export const dbConnection = async () => {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.DATABASE_URL, {
-      bufferCommands: false,
-      maxPoolSize: 2,
-    });
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log('Database connection established');
+  } catch (error) {
+    console.log(error);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 };
