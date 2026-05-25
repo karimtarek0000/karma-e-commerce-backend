310df64fa938489d641c1e27d6c502623c45f908
Karim Tarek abdel aziz <43219770+karimtarek0000@users.noreply.github.com>
2026-05-25 17:34:07 +0300

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,13 +1,11 @@
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.DATABASE_URL;
-
 if (!MONGODB_URI) {
   throw new Error('Please define the DATABASE_URL environment variable inside .env');
 }
 
 let cached = global.mongoose;
-
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
@@ -17,20 +15,21 @@ export const dbConnection = async () => {
     return cached.conn;
   }
 
+  if (mongoose.connection.readyState === 0) {
+    cached.conn = null;
+    cached.promise = null;
+  }
+
   if (!cached.promise) {
     const opts = {
-      bufferCommands: false,
+      bufferCommands: true,
       maxPoolSize: 2,
       serverSelectionTimeoutMS: 8000,
       connectTimeoutMS: 10000,
       socketTimeoutMS: 45000,
     };
-
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongooseInstance) => {
       return mongooseInstance;
-    }).catch((err) => {
-      cached.promise = null;
-      throw err;
     });
   }
 
@@ -41,6 +40,5 @@ export const dbConnection = async () => {
     cached.conn = null;
     throw e;
   }
-
   return cached.conn;
 };
